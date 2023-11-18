6914ed4c552eb0b561208538bd3b00e8b4c116a6
hydazz <53986978+hydazz@users.noreply.github.com>
2023-11-18 21:16:33 +1100

    add microsoft suite

---
+++ Processors/Microsoft365VersionerGetter.py
@@ -0,0 +1,60 @@
+#!/usr/local/autopkg/python
+
+from __future__ import absolute_import
+import xml.etree.ElementTree as ET
+from autopkglib import Processor, ProcessorError, URLGetter
+
+__all__ = ["Microsoft365VersionerGetter"]
+
+FEED_URL = "https://macadmins.software/latest.xml"
+
+
+class Microsoft365VersionerGetter(URLGetter):
+    """Provides the version and download URL of the specified package"""
+
+    input_variables = {
+        "package_id": {
+            "description": "ID of the package to look for.",
+            "required": True,
+        }
+    }
+    output_variables = {
+        "version": {
+            "description": "Version of the specified package.",
+        },
+        "download_url": {
+            "description": "Download URL of the specified package.",
+        }
+    }
+    description = __doc__
+
+    def get_package_info(self, feed_url, package_id):
+        """Parse the XML feed for the specified package information"""
+        try:
+            xml = self.download(feed_url)
+        except Exception as e:
+            raise ProcessorError("Can't download %s: %s" % (feed_url, e))
+
+        root = ET.fromstring(xml)
+        for package in root.iter('package'):
+            if package.find('id').text == package_id:
+                download_url = package.find('download').text
+                version = package.find('cfbundleversion').text
+                return download_url, version
+
+        raise ProcessorError(
+            "Package ID not found in the feed: %s" % package_id)
+
+    def main(self):
+        package_id = self.env["package_id"]
+        download_url, version = self.get_package_info(FEED_URL, package_id)
+
+        self.env["version"] = version
+        self.env["download_url"] = download_url
+        self.output("Found Package: Version %s, Download URL: %s" %
+                    (version, download_url))
+
+
+if __name__ == "__main__":
+    processor = Microsoft365VersionerGetter()
+    processor.execute_shell()
---
+++ templater.py
@@ -0,0 +1,107 @@
+import os
+
+# Function to create a directory
+def create_directory(dir_name):
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    else:
+        print(f"Directory {dir_name} already exists.")
+
+# Function to create a file with given content
+def create_file(file_path, content):
+    with open(file_path, "w") as file:
+        file.write(content)
+
+# Main script
+def main():
+    app_name = input(
+        "Enter the application name (e.g., 'Autodesk Fusion 360'): ").strip()
+    dir_name = app_name.replace(" ", "")
+    create_directory(dir_name)
+
+    # Content for AutodeskFusion360.pkg.recipe.yaml
+    pkg_recipe_content = f"""Identifier: com.github.woodleighschool.pkg.{dir_name}
+ParentRecipe: com.github.woodleighschool.download.{dir_name}
+
+Input:
+  NAME: {app_name}
+
+Process:
+"""
+    create_file(os.path.join(
+        dir_name, f"{dir_name}.pkg.recipe.yaml"), pkg_recipe_content)
+
+    # Content for AutodeskFusion360.jamf.recipe.yaml
+    jamf_recipe_content = f"""Identifier: com.github.woodleighschool.jamf.{dir_name}
+ParentRecipe: com.github.woodleighschool.pkg.{dir_name}
+
+Input:
+  NAME: {app_name}
+  CATEGORY: <Category>
+  POLICY_CATEGORY: Self Service
+  POLICY_NAME: ALL - %NAME% - ALL
+  POLICY_TEMPLATE: defaultAllScope.xml
+  SELF_SERVICE_DISPLAY_NAME: "%NAME%"
+  SELF_SERVICE_DESCRIPTION: >
+    <Description>
+
+    Version: %version%
+  SELF_SERVICE_ICON: "%NAME%.png"
+  INSTALL_BUTTON_TEXT: Install
+  REINSTALL_BUTTON_TEXT: Reinstall
+  UPDATE_PREDICATE: pkg_uploaded == False
+
+Process:
+  - Processor: StopProcessingIf
+    Arguments:
+      predicate: "%UPDATE_PREDICATE%"
+
+  - Processor: com.github.grahampugh.jamf-upload.processors/JamfCategoryUploader
+    Arguments:
+      category_name: "%CATEGORY%"
+
+  - Processor: com.github.grahampugh.jamf-upload.processors/JamfPackageUploader
+    Arguments:
+      pkg_category: "%CATEGORY%"
+
+  - Processor: com.github.grahampugh.jamf-upload.processors/JamfPolicyUploader
+    Arguments:
+      icon: "%SELF_SERVICE_ICON%"
+      policy_name: "%POLICY_NAME%"
+      policy_template: "%POLICY_TEMPLATE%"
+      replace_policy: "True"
+      replace_icon: "True"
+
+  - Processor: com.github.grahampugh.jamf-upload.processors/JamfPackageCleaner
+    Arguments:
+      pkg_name_match: "%NAME%"
+      versions_to_keep: 1
+"""
+    create_file(os.path.join(
+        dir_name, f"{dir_name}.jamf.recipe.yaml"), jamf_recipe_content)
+
+    # Content for AutodeskFusion360.download.recipe.yaml
+    download_recipe_content = f"""Identifier: com.github.woodleighschool.download.{dir_name}
+
+Input:
+  NAME: {app_name}
+  DOWNLOAD_URL: ""
+
+Process:
+  - Processor: URLDownloader
+    Arguments:
+      url: "%DOWNLOAD_URL%"
+      filename: "%NAME%.pkg"
+
+  - Processor: EndOfCheckPhase
+
+  - Processor: CodeSignatureVerifier
+    Arguments:
+      input_path: "%pathname%"
+"""
+    create_file(os.path.join(
+        dir_name, f"{dir_name}.download.recipe.yaml"), download_recipe_content)
+
+
+if __name__ == "__main__":
+    main()
