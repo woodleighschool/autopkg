d61d6e4ce9b7a4741aa8395b75be9e28137530d8
hydazz <alexanderhyde@icloud.com>
2025-03-24 14:46:38 +1100

    new login background, remove vscode extentions

--- OutsetScripts/Assets/outset/login-once/Apply-Branding.sh
+++ OutsetScripts/Assets/outset/login-once/Apply-Branding.sh
@@ -6,59 +6,62 @@ until pgrep -q -x "Dock"; do
 	sleep 1
 done
 
-# Set Wallpaper
-echo "Applying wallpaper"
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Woodleigh.jpg"'
+if ! defaults read io.macadmins.Outset run_once | grep -q 'ApplyBranding.sh'; then
 
-# Function to format the dock item XML
-dock_item() {
-	printf '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>%s</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>', "$1"
-}
+	# Set Wallpaper
+	echo "Applying wallpaper"
+	osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Woodleigh.jpg"'
 
-# Function to setup Dock
-setup_dock() {
-	defaults delete com.apple.dock persistent-apps
+	# Function to format the dock item XML
+	dock_item() {
+		printf '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>%s</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>', "$1"
+	}
 
-	if [ -d "/Applications/Microsoft Teams.app" ]; then
-		TEAMSPATH="/Applications/Microsoft Teams.app"
-	elif [ -d "/Applications/Microsoft Teams (work or school).app" ]; then
-		TEAMSPATH="/Applications/Microsoft Teams (work or school).app"
-	elif [ -d "/Applications/Microsoft Teams classic.app" ]; then
-		TEAMSPATH="/Applications/Microsoft Teams classic.app"
-	fi
+	# Function to setup Dock
+	setup_dock() {
+		defaults delete com.apple.dock persistent-apps
 
-	defaults write com.apple.dock persistent-apps -array \
-		"$(dock_item /System/Applications/Launchpad.app)" \
-		"$(dock_item /System/Applications/Mission\ Control.app)" \
-		"$(dock_item /Applications/Google\ Chrome.app)" \
-		"$(dock_item /Applications/Microsoft\ Word.app)" \
-		"$(dock_item /Applications/Microsoft\ Powerpoint.app)" \
-		"$(dock_item /Applications/Microsoft\ Excel.app)" \
-		"$(dock_item /Applications/Microsoft\ Outlook.app)" \
-		"$(dock_item /Applications/Microsoft\ OneNote.app)" \
-		"$(dock_item "${TEAMSPATH}")" \
-		"$(dock_item /System/Applications/System\ Settings.app)" \
-		"$(dock_item /Applications/Self\ Service.app)" \
-		"$(dock_item /Applications/Vivi.app)"
+		if [ -d "/Applications/Microsoft Teams.app" ]; then
+			TEAMSPATH="/Applications/Microsoft Teams.app"
+		elif [ -d "/Applications/Microsoft Teams (work or school).app" ]; then
+			TEAMSPATH="/Applications/Microsoft Teams (work or school).app"
+		elif [ -d "/Applications/Microsoft Teams classic.app" ]; then
+			TEAMSPATH="/Applications/Microsoft Teams classic.app"
+		fi
 
-	defaults write com.apple.dock minimize-to-application -bool true
-	defaults write com.apple.dock show-recents -bool false
+		defaults write com.apple.dock persistent-apps -array \
+			"$(dock_item /System/Applications/Launchpad.app)" \
+			"$(dock_item /System/Applications/Mission\ Control.app)" \
+			"$(dock_item /Applications/Google\ Chrome.app)" \
+			"$(dock_item /Applications/Microsoft\ Word.app)" \
+			"$(dock_item /Applications/Microsoft\ Powerpoint.app)" \
+			"$(dock_item /Applications/Microsoft\ Excel.app)" \
+			"$(dock_item /Applications/Microsoft\ Outlook.app)" \
+			"$(dock_item /Applications/Microsoft\ OneNote.app)" \
+			"$(dock_item "${TEAMSPATH}")" \
+			"$(dock_item /System/Applications/System\ Settings.app)" \
+			"$(dock_item /Applications/Self\ Service.app)" \
+			"$(dock_item /Applications/Vivi.app)"
 
-	killall Dock
-}
+		defaults write com.apple.dock minimize-to-application -bool true
+		defaults write com.apple.dock show-recents -bool false
 
-# Attempt to setup Dock
-setup_dock
-sleep 1
+		killall Dock
+	}
 
-# Just do what you're told to do, Dock!
-for i in {1..5}; do
-	sleep 5
-	if defaults read com.apple.dock persistent-apps | grep -qi "Self Service"; then
-		echo "Dock setup confirmed."
-		break
-	else
-		echo "Dock setup not confirmed, retrying."
-		setup_dock
-	fi
-done
+	# Attempt to setup Dock
+	setup_dock
+	sleep 1
+
+	# Just do what you're told to do, Dock!
+	for i in {1..5}; do
+		sleep 5
+		if defaults read com.apple.dock persistent-apps | grep -qi "Self Service"; then
+			echo "Dock setup confirmed."
+			break
+		else
+			echo "Dock setup not confirmed, retrying."
+			setup_dock
+		fi
+	done
+fi
